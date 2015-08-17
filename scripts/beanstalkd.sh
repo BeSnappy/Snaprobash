b87cb3eda2d5a7d069194be3a7d8a054a560d37a
Patrick Leckey <pat.leckey@gmail.com>
2015-08-16 21:12:06 -0400

    updated beanstalkd to listen on 0.0.0.0

--- scripts/beanstalkd.sh
+++ scripts/beanstalkd.sh
@@ -8,6 +8,7 @@ sudo apt-get install -qq beanstalkd
 
 # Set to start on system start
 sudo sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
+sudo sed -i "s/BEANSTALKD_LISTEN_ADDR=127\.0\.0\.1/BEANSTALKD_LISTEN_ADDR=0\.0\.0\.0/" /etc/default/beanstalkd
 
 # Start Beanstalkd
 sudo service beanstalkd start
